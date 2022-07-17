8d63261e001171e6cd95f855d0efebe4ffee598b
Alexander Davidenko <gypsyman534@gmail.com>
2022-07-17 12:27:09 +0300

    set remarks for styles files

--- src/app/mock/mock-data.ts
+++ src/app/mock/mock-data.ts
@@ -5,4 +5,24 @@ export const topicCardList: Card[] = [
     cardId: 1,
     cardTitle: 'Routing',
   },
+  {
+    cardId: 2,
+    cardTitle: '???',
+  },
+  {
+    cardId: 3,
+    cardTitle: '???',
+  },
+  {
+    cardId: 4,
+    cardTitle: '???',
+  },
+  {
+    cardId: 5,
+    cardTitle: '???',
+  },
+  {
+    cardId: 6,
+    cardTitle: '???',
+  },
 ];
